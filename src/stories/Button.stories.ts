99664f915e9911eb23f2c7813d30d6c8afbf8aa5
Drops & Bits <6819071+dropsandbits@users.noreply.github.com>
2022-09-19 21:54:27 +0200

    Cleanup

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -21,22 +21,22 @@ export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/angular/writing-stories/args
 Primary.args = {
   primary: true,
-  label: 'Default CTA',
+  label: 'Miti Button',
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  label: 'Default CTA',
+  label: 'Miti Button',
 };
 
 export const Large = Template.bind({});
 Large.args = {
   size: 'large',
-  label: 'Default CTA',
+  label: 'Miti Button',
 };
 
 export const Small = Template.bind({});
 Small.args = {
   size: 'small',
-  label: 'Default CTA',
+  label: 'Miti Button',
 };
